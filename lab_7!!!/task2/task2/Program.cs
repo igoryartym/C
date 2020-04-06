cb1256786691542154e7abbc21712c5c6f780a5c
unknown <igoryartym66@gmail.com>
2020-04-06 14:45:27 +0300

    lab

---
+++ lab_7!!!/task2/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static double Matrics(int i, int j, int x)
+        {
+            return i * (Math.Sin(i * x) + Math.Cos(j * x));
+
+        }
+        static void Main(string[] args)
+        {
+            int n, m, x;
+            Console.Write("К-ть рядкiв =");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("К-ть стовпцiв =");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("x =");
+            x = Convert.ToInt32(Console.ReadLine());
+            double[,] A = new double[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    A[i, j] = Matrics(i, j, x);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    if (A[i, j] < x)
+                        Console.WriteLine($"{A[i, j]}");
+            }
+        }
+    }
+}
---
+++ lab_7!!!/task3/task3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m,k=-1;
+            Console.Write("К-ть рядкiв: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("К-ть стовцiв: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            int[,] A = new int[n, m];
+            int[] Vect = new int[m];
+            int[,] B = new int[m, n];
+            int[] Answer = new int[m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("A[{0},{1}]= ", i, j);
+                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write($"Введiть вект:{i}= ");
+                Vect[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    B[j, i] = A[i, j];
+            }
+            for (int i = 0; i < m; i++)
+            {
+                k++;
+                if (k == m) break;
+                for (int j = 0; j < n; j++)
+                    Answer[i] = Vect[k] * B[j, i];
+            }
+            for (int i = 0; i < m; i++)
+                Console.WriteLine(Answer[i]);
+        }
+    }
+}
